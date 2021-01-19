06b3c8cad381ec8c29ef165e8df31ba840a61718
Grégory <grg.vilar@gmail.com>
2021-01-19 20:21:12 +0100

    mise en place du raffraichissement de la page

--- script.js
+++ script.js
@@ -1,17 +1,39 @@
 window.onload = function () {
-    const canvas = document.createElement('canvas');
-    // Permet de créer l'élément Canvas
-    canvas.width = 1420;
-    canvas.height = 800;
-    canvas.style.border = "5px solid green";
-    // Attribution du style de mon canvas
-    document.body.appendChild(canvas);
-    // Ici on rattache le canvas au body du docuement html
 
-    var context = canvas.getContext('2d')
-    // Permet d'écrire dans le context en 2D
-    context.fillStyle = "#ff0000"
-    // Permet de choisir la couleur avec laquelle je vais dessiner
-    context.fillRect(30, 30, 100, 50)
-    // Création du rectangle
+    var canvas;
+    var context;
+    var delay = 100;
+    var xPosition = 0;
+    var Yposition = 0;
+
+    init();
+
+    function init() {
+         canvas = document.createElement('canvas');
+        // Permet de créer l'élément Canvas
+        canvas.width = 1420;
+        canvas.height = 800;
+        canvas.style.border = "5px solid black";
+        // Attribution du style de mon canvas
+        document.body.appendChild(canvas);
+        // Ici on rattache le canvas au body du docuement html      
+        context = canvas.getContext('2d')
+        // Permet d'écrire dans le context en 2D
+        refreshCanvas();
+        }
+
+    function refreshCanvas () {
+        xPosition += 2;
+        Yposition += 2;
+
+        context.fillStyle = "#ff0000"
+        // Permet de choisir la couleur avec laquelle je vais dessiner
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        // Permet d'effacer le rectangle avant de le recréer, ce qui donne une impression de mouvement.
+        context.fillRect(xPosition, Yposition, 100, 50);
+        // Création du rectangle
+        setTimeout(refreshCanvas, delay)
+    }
+
+
 }
